beac7627a0f9e8d762d9520b268a5c05e3f249cc
Gabriela <129890072+martinsgabi@users.noreply.github.com>
2023-04-05 11:54:45 -0300

    alteração calculadora

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener( "click" , showcolor );
 
 document.getElementById( "botao" ).addEventListener( "click" , exibenome );
 
+document.getElementById( "btn-soma" ).addEventListener( "click" , showresultado );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -12,5 +14,10 @@ function exibenome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é: " + usuario;
     document.getElementById( "nome" ).value = "";
-   
+}
+
+function showresultado()
+{
+    let resultado = document.getElementById( "number" ).value;
+    document.getElementById( "resultado1" ).innerHTML = "=" + resultado;
 }
