8c6f64462218ff2b3c60c6690e7ff8f19fcf0f45
Gabriela <129890072+martinsgabi@users.noreply.github.com>
2023-04-05 11:26:59 -0300

    alteração valor

--- main.js
+++ main.js
@@ -10,5 +10,6 @@ function showcolor()
 function exibenome()
 {
     let usuario = document.getElementById( "nome" ).value;
-    document.getElementById( "mensagem" ).innerText = "Meu nome é: " + usuario
+    document.getElementById( "mensagem" ).innerText = "Meu nome é: " + usuario;
+    let usuario = document.getElementById( "nome" ).value = "";
 }
